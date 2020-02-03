e2ae811d5747bfa0ce759eb45de87ae799b16e79
ben <benjamin.n.chapin@gmail.com>
2020-02-03 10:48:37 -0600

    adds genres and moviegenres models and tables unsuccessfully

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-actor = Actor.create(first_name: "Will", last_name: "Ferrell", gender: "male", age: "52", known_for: "Stepbrothers", movie_id: 1)
-actor.save
+# actor = Actor.create(first_name: "Will", last_name: "Ferrell", gender: "male", age: "52", known_for: "Stepbrothers", movie_id: 1)
+# actor.save
 
-actor = Actor.create(first_name: "Arnold", last_name: "Schwarzenegger", gender: "male", age: "72", known_for: "The Terminator", movie_id: 13)
-actor.save
+# actor = Actor.create(first_name: "Arnold", last_name: "Schwarzenegger", gender: "male", age: "72", known_for: "The Terminator", movie_id: 13)
+# actor.save
 
-actor = Actor.create(first_name: "Keanu", last_name: "Reeves", gender: "male", age: "55", known_for: "John Wick", movie_id: 1)
-actor.save
+# actor = Actor.create(first_name: "Keanu", last_name: "Reeves", gender: "male", age: "55", known_for: "John Wick", movie_id: 1)
+# actor.save
 
-actor = Actor.create(first_name: "Will", last_name: "Ferrell", gender: "male", age: "52", known_for: "Stepbrothers", movie_id: 1)
-actor.save
+# actor = Actor.create(first_name: "Will", last_name: "Ferrell", gender: "male", age: "52", known_for: "Stepbrothers", movie_id: 1)
+# actor.save
 
 
+# Genre.create({name: "comedy"})
+
+# Genre.create({name: "action"})
+
+# Genre.create({name: "romcom"})
+
+# Genre.create({name: "horror"})
+
+
+# MovieGenre.create!([
+#   {movie_id: 1, genre_id: 1},
+#   {movie_id: 2, genre_id: 2},
+#   {movie_id: 4, genre_id: 3},
+#   {movie_id: 6, genre_id: 4},
+#   {movie_id: 11, genre_id: 3},
+#   {movie_id: 13, genre_id: 2},
+#   {movie_id: 14, genre_id: 2},
+#   {movie_id: 5, genre_id: 4},
+# ])
